4ecc6e8753e721f6ecf37be3ffaa0c6c9f1e67b7
DESKTOP-UMQKT1R\fontb <font.benjamin@yahoo.fr>
2020-09-29 20:41:58 +0200

    Added the website route calculator + modified the welcome fragment menu icon

--- app/src/main/java/com/example/lokakaka/tradingFragment/TradingFragment.kt
+++ app/src/main/java/com/example/lokakaka/tradingFragment/TradingFragment.kt
@@ -4,16 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.lokakaka.R
 
 class TradingFragment: Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+    private lateinit var webView: WebView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
         inflater.inflate(R.layout.fragment_trading, container, false)
+
 
     companion object {
         fun newInstance(): TradingFragment = TradingFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        webView = getView()?.findViewById(R.id.tradingWebView)!!
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        webView.loadUrl(getString(R.string.trading_calculator_url))
     }
 
 }
