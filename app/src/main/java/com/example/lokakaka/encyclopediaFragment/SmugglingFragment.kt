c6475b95de28ae716465004705888192f35df44f
DESKTOP-UMQKT1R\fontb <font.benjamin@yahoo.fr>
2020-10-05 15:20:34 +0200

    Back arrow and categories layout finished

--- app/src/main/java/com/example/lokakaka/encyclopediaFragment/SmugglingFragment.kt
+++ app/src/main/java/com/example/lokakaka/encyclopediaFragment/SmugglingFragment.kt
@@ -5,30 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.lokakaka.R
 
-
-class EncyclopediaFragment: Fragment() {
-
-    private lateinit var smugglingButton: Button
+class SmugglingFragment: Fragment() {
+    private lateinit var backImageView: ImageView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_encyclopedia, container, false)
+        inflater.inflate(R.layout.fragment_smuggling, container, false)
 
     companion object {
-        fun newInstance(): EncyclopediaFragment = EncyclopediaFragment()
+        fun newInstance(): SmugglingFragment = SmugglingFragment()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        smugglingButton = getView()?.findViewById(R.id.buttonSmuggling)!!
-        smugglingButton.setOnClickListener( {
+        backImageView = getView()?.findViewById(R.id.backImageView)!!
+        backImageView.setOnClickListener( {
             val ft: FragmentTransaction = fragmentManager!!.beginTransaction()
-            ft.replace(this.id, SmugglingFragment(), "SmugglingFragment")
+            ft.replace(this.id, EncyclopediaFragment(), "EncyclopediaFragment")
             ft.commit()
         })
     }
-
 }
